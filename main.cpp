4be65fc85b7c212b1f721399f99ccc29c1bf09db
Denis Adomich <zakifaki2005@gmail.com>
2025-09-06 16:20:35 +0300

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+use standart namespace;
+
+int main() {
+  cout << "Hello, my name is Denis\n";
+  cout << "2 + 2 = " 2 + 2 << endl;
+  cout << "I'is my commit!\n"; 
+  
+  return 0;
+}
